c8e74b849025db9b761809f1a7ee318f3ed65534
ThNam203 <hthnam203@gmail.com>
2024-08-27 15:31:16 +0700

    dev: add error not found page

---
+++ ui/app/not-found.tsx
@@ -0,0 +1,5 @@
+import GlobalErrorComponent from "@/components/errors/GlobalError";
+
+export default function NotFound() {
+  return <GlobalErrorComponent type="404" />
+}
--- ui/components/errors/GlobalError.tsx
+++ ui/components/errors/GlobalError.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import styles from './GlobalError.module.css'; // Adjust the path as necessary
+"use client";
+import React from "react";
+import styles from "./GlobalError.module.css";
 
 type GlobalErrorProps = {
-    error: Error & { digest?: string },
-    reset: () => void
-}
+    error?: Error & { digest?: string };
+    reset?: () => void;
+    type: "404" | "500";
+};
 
-const GlobalError = (props: GlobalErrorProps) => {
+const GlobalErrorComponent = (props: GlobalErrorProps) => {
     return (
         <section className={styles.wrapper}>
             <div className={styles.container}>
-                <div id="scene" className={styles.scene} data-hover-only="false">
-                    <div className={styles.circle} data-depth="1.2"></div>
+                <div id="scene" className={styles.scene}>
+                    <div className={styles.circle}></div>
 
-                    <div className={styles.one} data-depth="0.9">
+                    <div className={styles.one}>
                         <div className={styles.content}>
                             <span className={styles.piece}></span>
                             <span className={styles.piece}></span>
@@ -21,7 +23,7 @@ const GlobalError = (props: GlobalErrorProps) => {
                         </div>
                     </div>
 
-                    <div className={styles.two} data-depth="0.60">
+                    <div className={styles.two}>
                         <div className={styles.content}>
                             <span className={styles.piece}></span>
                             <span className={styles.piece}></span>
@@ -29,25 +31,37 @@ const GlobalError = (props: GlobalErrorProps) => {
                         </div>
                     </div>
 
-                    <div className={styles.three} data-depth="0.40">
+                    <div className={styles.three}>
                         <div className={styles.content}>
                             <span className={styles.piece}></span>
                             <span className={styles.piece}></span>
                             <span className={styles.piece}></span>
                         </div>
                     </div>
-
-                    <p className={styles.p404} data-depth="0.50">500</p>
-                    <p className={styles.p404} data-depth="0.10">500</p>
+                    <p className={styles.p404}>{props.type}</p>
                 </div>
 
                 <div className={styles.text}>
                     <article className={styles.article}>
                         <p>
-                            Something went wrong!<br />
-                            {props.error.digest ? props.error.digest : props.error.message}
+                            {props.type === "500"
+                                ? "Something went wrong!"
+                                : "Page not found"}
+                            <br />
+                            {props.type === "500" && props.error?.digest
+                                ? props.error.digest
+                                : props.type === "500" && props.error?.message
+                                ? props.error.message
+                                : null}
                         </p>
-                        <button onClick={props.reset}>Try again</button>
+                        {props.type === "500" && (
+                            <button onClick={props.reset}>Try again</button>
+                        )}
+                        {props.type === "404" && (
+                            <button onClick={() => window.history.back()}>
+                                Go back
+                            </button>
+                        )}
                     </article>
                 </div>
             </div>
@@ -55,4 +69,4 @@ const GlobalError = (props: GlobalErrorProps) => {
     );
 };
 
-export default GlobalError;
+export default GlobalErrorComponent;
