b1c2e7b0888461ea989b8c9b6ff8834b5da73b4f
ThNam203 <hthnam203@gmail.com>
2024-10-15 22:30:53 +0700

    add a way to run ipfs as bootstrap or normal node

--- ipfs-impl/main.go
+++ ipfs-impl/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"context"
+	//"errors"
+	"flag"
 	"fmt"
 	"log"
-	"os"
 
-	"github.com/ipfs/go-cid"
-	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/multiformats/go-multiaddr"
 )
@@ -20,98 +19,133 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	ds := NewInMemoryDatastore()
-	host, dht, err := NewLibp2pHost(ctx, ds)
-	if err != nil {
-		panic(err)
-	}
+	var bootstrapNodeAddr string
 
-	ipfsNode, err = NewIPFSNode(ctx, ds, host, dht)
-	if err != nil {
-		panic(err)
-	}
+	isBootstrapNode := flag.Bool("t", false, "Use if the node is a bootstrap node")
+	flag.StringVar(&bootstrapNodeAddr, "a", "", "The boostrap node address")
 
-	hostAddr, _ := multiaddr.NewMultiaddr(fmt.Sprintf("/p2p/%s", ipfsNode.host.ID().String()))
-	addr := ipfsNode.host.Addrs()[0]
-	log.Printf("IPFS node run on: %s", addr.Encapsulate(hostAddr))
+	flag.Parse()
+
+	if *isBootstrapNode {
+		if err := RunBootstrapNode(ctx); err != nil {
+			panic(err)
+		}
+	} else {
+		if len(bootstrapNodeAddr) == 0 {
+			log.Panic("missing bootstrap node address")
+		}
+
+		if err := RunNormalNode(ctx, bootstrapNodeAddr); err != nil {
+			log.Panic(err)
+		}
+	}
 
 	select {}
 }
 
-// local check lol
-func runExample(ctx context.Context, host host.Host) {
-	//fileCid, _ := addFileToNode(ctx)
-	//getFileFromNode(ctx)
-	//GetFileFromCID(ctx, fileCid)
-
-	targetAddr, err := multiaddr.NewMultiaddr("/ip4/127.0.0.1/tcp/4001/p2p/12D3KooWC5uA1QUEXmKnceznhJetH8G4wKrmhgqzXGgTGhZUDvY5")
+func RunBootstrapNode(ctx context.Context) error {
+	ds := NewInMemoryDatastore()
+	host, dht, err := NewLibp2pHost(ctx, ds)
 	if err != nil {
-		panic(err)
+		return err
+	}
+
+	ipfsNode, err = NewIPFSNode(ctx, ds, host, dht)
+	if err != nil {
+		return err
+	}
+
+	hostAddr, _ := multiaddr.NewMultiaddr(fmt.Sprintf("/p2p/%s", ipfsNode.host.ID().String()))
+	addr := ipfsNode.host.Addrs()[0]
+	log.Println("running as bootstrap node, ignore -a flag if there is any")
+	log.Printf("** bootstrap node address: %s\n", addr.Encapsulate(hostAddr))
+	return nil
+}
+
+func RunNormalNode(ctx context.Context, bootstrapNodeAddr string) error {
+	// parse the bootstrap node address
+	targetAddr, err := multiaddr.NewMultiaddr(bootstrapNodeAddr)
+	if err != nil {
+		return err
 	}
 
 	targetInfo, err := peer.AddrInfoFromP2pAddr(targetAddr)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	err = host.Connect(ctx, *targetInfo)
+	// create node
+	ds := NewInMemoryDatastore()
+	host, dht, err := NewLibp2pHost(ctx, ds)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	log.Println("Connected to", targetInfo.ID)
-	GetFileFromCID(ctx, "QmdbNULb6QosrqAZyKgGWMR6rw1szcKG3vZmuNwi7QMFvq")
+	ipfsNode, err = NewIPFSNode(ctx, ds, host, dht)
+	if err != nil {
+		return err
+	}
 
+	// connect to bootstrap node
+	log.Printf("running as normal node, connecting to bootstrap node addr %s\n", bootstrapNodeAddr)
+	if err := host.Connect(ctx, *targetInfo); err != nil {
+		return err
+	}
+	log.Println("connected successfully")
+
+	return nil
 }
 
-func GetFileFromCID(ctx context.Context, fileCid string) {
-	c, err := cid.Decode(fileCid)
-	if err != nil {
-		log.Printf("invalid CID: %s\n", err)
-		return
-	}
+// TESTING FUNCTIONS - TODO: write unit tests
 
-	rsc, err := ipfsNode.GetFile(ctx, c)
-	if err != nil {
-		log.Printf("failed to get file from node: %s\n", err)
-		return
-	}
-
-	defer rsc.Close()
-	log.Println("file successfully retrieved from the node!")
-}
-
-func getFileFromNode(ctx context.Context) {
-	fileCid := "QmPtU9NDfdxFB2oRiE4Lv37i4zWgVPme7qjTqfhZZ18Z89"
-	c, err := cid.Decode(fileCid)
-	if err != nil {
-		log.Printf("invalid CID: %s\n", err)
-		return
-	}
-	rsc, err := ipfsNode.GetFile(ctx, c)
-
-	if err != nil {
-		log.Printf("failed to get file from node: %s\n", err)
-		return
-	}
-
-	defer rsc.Close()
-	log.Println("file successfully retrieved from the node!")
-}
-
-func addFileToNode(ctx context.Context) (fileCid string, err error) {
-	file, err := os.Open("./example_file_to_be_added.txt")
-	if err != nil {
-		log.Printf("failed to open file: %s\n", err)
-		return "", err
-	}
-
-	ipldNode, err := ipfsNode.AddFile(ctx, file)
-	if err != nil {
-		log.Printf("failed to save file into node: %s\n", err)
-		return "", err
-	}
-
-	log.Printf("saved a file with cid: %s", ipldNode.Cid().String())
-	return ipldNode.String(), nil
-}
+// func GetFileFromCID(ctx context.Context, fileCid string) {
+// 	c, err := cid.Decode(fileCid)
+// 	if err != nil {
+// 		log.Printf("invalid CID: %s\n", err)
+// 		return
+// 	}
+//
+// 	rsc, err := ipfsNode.GetFile(ctx, c)
+// 	if err != nil {
+// 		log.Printf("failed to get file from node: %s\n", err)
+// 		return
+// 	}
+//
+// 	defer rsc.Close()
+// 	log.Println("file successfully retrieved from the node!")
+// }
+//
+// func getFileFromNode(ctx context.Context) {
+// 	fileCid := "QmPtU9NDfdxFB2oRiE4Lv37i4zWgVPme7qjTqfhZZ18Z89"
+// 	c, err := cid.Decode(fileCid)
+// 	if err != nil {
+// 		log.Printf("invalid CID: %s\n", err)
+// 		return
+// 	}
+// 	rsc, err := ipfsNode.GetFile(ctx, c)
+//
+// 	if err != nil {
+// 		log.Printf("failed to get file from node: %s\n", err)
+// 		return
+// 	}
+//
+// 	defer rsc.Close()
+// 	log.Println("file successfully retrieved from the node!")
+// }
+//
+// func addFileToNode(ctx context.Context) (fileCid string, err error) {
+// 	file, err := os.Open("./example_file_to_be_added.txt")
+// 	if err != nil {
+// 		log.Printf("failed to open file: %s\n", err)
+// 		return "", err
+// 	}
+//
+// 	ipldNode, err := ipfsNode.AddFile(ctx, file)
+// 	if err != nil {
+// 		log.Printf("failed to save file into node: %s\n", err)
+// 		return "", err
+// 	}
+//
+// 	log.Printf("saved a file with cid: %s", ipldNode.Cid().String())
+// 	return ipldNode.String(), nil
+// }
