a43e405ef5c47825f9c3a66cf32582bc226dc3d2
ThNam203 <hthnam203@gmail.com>
2025-03-13 11:20:10 +0700

    add livestream instruction

--- ui/app/(main)/page.tsx
+++ ui/app/(main)/page.tsx
@@ -1,11 +1,17 @@
 import LivestreamsPreviewView from "../../components/main_page/LivesteamsPreviewView";
 import { PopularVODView } from "../../components/main_page/popular-vod-view";
 
-
 export default function HomePage() {
     return (
-        <div className="flex flex-col w-full max-h-full px-8 py-4 overflow-y-scroll overflow-x-hidden">
-            <h1 className="font-semibold text-xl mb-2">Livestreaming</h1>
+        <div className="flex flex-col w-full max-h-full px-8 py-4 overflow-y-scroll overflow-x-hidden text-xs">
+            <h1 className="font-semibold text-xl">Livestreaming</h1>
+            <p>How to start your livestream: </p>
+            <p>Open OBS &rarr; Settings &rarr; Stream </p>
+            <p>
+                Enter: &quot;Server: rtmp://sen1or-huly.com:1935, StreamKey:
+                Your key in Security Setting&quot;
+            </p>
+            <p>Start livestream</p>
             <LivestreamsPreviewView />
 
             <h1 className="font-semibold text-xl my-2">Popular VODs</h1>
