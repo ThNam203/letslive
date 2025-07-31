f65915245f0656286010a93b478112b189abbc20
ThNam203 <hthnam203@gmail.com>
2025-08-01 00:43:57 +0700

    feat: update setting layout

--- ui/app/(main)/settings/profile/page.tsx
+++ ui/app/(main)/settings/profile/page.tsx
@@ -5,9 +5,9 @@ import { toast } from "react-toastify";
 import { Button } from "../../../../components/ui/button";
 import useUser from "../../../../hooks/user";
 import {
-  UpdateBackgroundPicture,
-  UpdateProfile,
-  UpdateProfilePicture,
+    UpdateBackgroundPicture,
+    UpdateProfile,
+    UpdateProfilePicture,
 } from "../../../../lib/api/user";
 import TextField from "../_components/text-field";
 import ProfileBanner from "./_components/profile-banner";
@@ -18,189 +18,191 @@ import ThemeList from "@/components/utils/theme-list";
 import IconLoader from "@/components/icons/loader";
 
 export default function ProfileSettings() {
-  const user = useUser((state) => state.user);
-  const updateUser = useUser((state) => state.updateUser);
+    const user = useUser((state) => state.user);
+    const updateUser = useUser((state) => state.updateUser);
 
-  const [displayName, setDisplayName] = useState("");
-  const [bio, setBio] = useState("");
-  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
-  const [isUpdatingProfile, setIsUpdatingProfile] = useState(false);
-  const [profileImageFile, setProfileImageFile] = useState<File | null>(null);
-  const [backgroundImageFile, setBackgroundImageFile] = useState<File | null>(
-    null
-  );
-
-  const handleProfileImageChange = (file: File | null) => {
-    setProfileImageFile(file);
-  };
-
-  const handleBackgroundImageChange = (file: File | null) => {
-    setBackgroundImageFile(file);
-  };
-
-  const handleUpdateProfileInformation = async (
-    event: React.FormEvent<HTMLFormElement>
-  ) => {
-    event.preventDefault();
-    setIsUpdatingProfile(true);
-
-    if (backgroundImageFile) {
-      const { fetchError: backgroundImageError, newPath: backgroundImagePath } =
-        await UpdateBackgroundPicture(backgroundImageFile).finally(() =>
-          setIsUpdatingProfile(false)
-        );
-      if (backgroundImageError) {
-        toast.error(backgroundImageError.message);
-        return;
-      }
-      setBackgroundImageFile(null);
-      updateUser({
-        ...user!,
-        backgroundPicture: backgroundImagePath,
-      });
-    }
-
-    if (profileImageFile) {
-      const { fetchError: profileImageError, newPath: profileImagePath } =
-        await UpdateProfilePicture(profileImageFile).finally(() =>
-          setIsUpdatingProfile(false)
-        );
-      if (profileImageError) {
-        toast.error(profileImageError.message);
-        return;
-      }
-      setProfileImageFile(null);
-      updateUser({
-        ...user!,
-        profilePicture: profileImagePath,
-      });
-    }
-
-    if (bio || displayName) {
-      const { updatedUser, fetchError } = await UpdateProfile({
-        ...user!,
-        displayName,
-        bio,
-      }).finally(() => setIsUpdatingProfile(false));
-      if (fetchError) {
-        toast.error(fetchError.message);
-        return;
-      }
-      updateUser({
-        ...user!,
-        ...updatedUser!,
-      });
-    }
-
-    toast.success("Profile information updated successfully!");
-  };
-
-  useEffect(() => {
-    if (!user) return;
-
-    const isDisplayNameChange = (user.displayName ?? "") !== displayName;
-    const isBioChange = (user.bio ?? "") !== bio;
-    const isProfileImageChange = profileImageFile !== null;
-    const isBackgroundImageChange = backgroundImageFile !== null;
-
-    console.log(
-      isDisplayNameChange,
-      isBioChange,
-      isProfileImageChange,
-      isBackgroundImageChange
+    const [displayName, setDisplayName] = useState("");
+    const [bio, setBio] = useState("");
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+    const [isUpdatingProfile, setIsUpdatingProfile] = useState(false);
+    const [profileImageFile, setProfileImageFile] = useState<File | null>(null);
+    const [backgroundImageFile, setBackgroundImageFile] = useState<File | null>(
+        null,
     );
 
-    const isUserDataChange =
-      isDisplayNameChange ||
-      isBioChange ||
-      isProfileImageChange ||
-      isBackgroundImageChange;
+    const handleProfileImageChange = (file: File | null) => {
+        setProfileImageFile(file);
+    };
 
-    if (isUserDataChange) setIsButtonDisabled(false);
-    else setIsButtonDisabled(true);
-  }, [displayName, bio, user, profileImageFile, backgroundImageFile]);
+    const handleBackgroundImageChange = (file: File | null) => {
+        setBackgroundImageFile(file);
+    };
 
-  useEffect(() => {
-    setDisplayName(user?.displayName ?? "");
-    setBio(user?.bio ?? "");
-  }, [user]);
+    const handleUpdateProfileInformation = async (
+        event: React.FormEvent<HTMLFormElement>,
+    ) => {
+        event.preventDefault();
+        setIsUpdatingProfile(true);
 
-  return (
-    <div className="min-h-screen text-foreground p-6 overflow-y-auto">
-      <div className="max-w-4xl space-y-8">
-        {/* Profile Settings Section */}
-        <Section
-          title="Profile Settings"
-          description="Change identifying details for your account"
-          hasBorder
-        >
-          <form className="space-y-6" onSubmit={handleUpdateProfileInformation}>
-            <ProfileBanner
-              className="mb-10"
-              onProfileImageChange={handleProfileImageChange}
-              onBackgroundImageChange={handleBackgroundImageChange}
-            />
-            <TextField
-              label="Username"
-              disabled
-              defaultValue={user?.username}
-            />
-            <TextField
-              label="Display Name"
-              defaultValue={user?.displayName}
-              value={displayName}
-              onChange={(e) => setDisplayName(e.target.value)}
-              disabled={isUpdatingProfile}
-            />
-            <TextAreaField
-              label="Bio"
-              defaultValue={user?.bio}
-              value={bio}
-              onChange={(e) => setBio(e.target.value)}
-              disabled={isUpdatingProfile}
-              className="min-h-[100px]"
-            />
+        if (backgroundImageFile) {
+            const {
+                fetchError: backgroundImageError,
+                newPath: backgroundImagePath,
+            } = await UpdateBackgroundPicture(backgroundImageFile).finally(() =>
+                setIsUpdatingProfile(false),
+            );
+            if (backgroundImageError) {
+                toast.error(backgroundImageError.message);
+                return;
+            }
+            setBackgroundImageFile(null);
+            updateUser({
+                ...user!,
+                backgroundPicture: backgroundImagePath,
+            });
+        }
 
-            <div className="flex justify-end mt-6">
-              <Button
-                disabled={isUpdatingProfile || isButtonDisabled}
-                type="submit"
-              >
-                {isUpdatingProfile && <IconLoader />} Save
-                Changes
-              </Button>
-            </div>
-          </form>
-        </Section>
+        if (profileImageFile) {
+            const { fetchError: profileImageError, newPath: profileImagePath } =
+                await UpdateProfilePicture(profileImageFile).finally(() =>
+                    setIsUpdatingProfile(false),
+                );
+            if (profileImageError) {
+                toast.error(profileImageError.message);
+                return;
+            }
+            setProfileImageFile(null);
+            updateUser({
+                ...user!,
+                profilePicture: profileImagePath,
+            });
+        }
 
-        <Section
-          title="Themes"
-          description="Customize the look and feel of the website"
-          className="border-t border-border pt-8"
-          contentClassName="border border-border rounded-md p-4"
-        >
-          <ThemeList/>
-        </Section>
+        if (bio || displayName) {
+            const { updatedUser, fetchError } = await UpdateProfile({
+                ...user!,
+                displayName,
+                bio,
+            }).finally(() => setIsUpdatingProfile(false));
+            if (fetchError) {
+                toast.error(fetchError.message);
+                return;
+            }
+            updateUser({
+                ...user!,
+                ...updatedUser!,
+            });
+        }
 
-        <Section
-          title="Disabling your Let's Live account"
-          description="Completely deactivate your account"
-          className="border-t border-border pt-8"
-          contentClassName="border border-border rounded-md p-4"
-        >
-          <div className="w-full flex items-center justify-between">
-            <p className="w-2/3 text-sm text-destructive">
-              When you disable your account, your profile and notifications will
-              be hidden, and your account will be deactivated. You can
-              reactivate your account at any time.
-            </p>
-            <button className="text-sm font-medium text-destructive-foreground bg-destructive px-4 py-2 rounded-md hover:bg-destructive-hover">
-              Disable Your Let&apos;s Live Account
-            </button>
-          </div>
-        </Section>
-        <div className="h-10"/>
-      </div>
-    </div>
-  );
+        toast.success("Profile information updated successfully!");
+    };
+
+    useEffect(() => {
+        if (!user) return;
+
+        const isDisplayNameChange = (user.displayName ?? "") !== displayName;
+        const isBioChange = (user.bio ?? "") !== bio;
+        const isProfileImageChange = profileImageFile !== null;
+        const isBackgroundImageChange = backgroundImageFile !== null;
+
+        console.log(
+            isDisplayNameChange,
+            isBioChange,
+            isProfileImageChange,
+            isBackgroundImageChange,
+        );
+
+        const isUserDataChange =
+            isDisplayNameChange ||
+            isBioChange ||
+            isProfileImageChange ||
+            isBackgroundImageChange;
+
+        if (isUserDataChange) setIsButtonDisabled(false);
+        else setIsButtonDisabled(true);
+    }, [displayName, bio, user, profileImageFile, backgroundImageFile]);
+
+    useEffect(() => {
+        setDisplayName(user?.displayName ?? "");
+        setBio(user?.bio ?? "");
+    }, [user]);
+
+    return (
+        <>
+            {/* Profile Settings Section */}
+            <Section
+                title="Profile Settings"
+                description="Change identifying details for your account"
+                hasBorder
+            >
+                <form
+                    className="space-y-6"
+                    onSubmit={handleUpdateProfileInformation}
+                >
+                    <ProfileBanner
+                        className="mb-10"
+                        onProfileImageChange={handleProfileImageChange}
+                        onBackgroundImageChange={handleBackgroundImageChange}
+                    />
+                    <TextField
+                        label="Username"
+                        disabled
+                        defaultValue={user?.username}
+                    />
+                    <TextField
+                        label="Display Name"
+                        defaultValue={user?.displayName}
+                        value={displayName}
+                        onChange={(e) => setDisplayName(e.target.value)}
+                        disabled={isUpdatingProfile}
+                    />
+                    <TextAreaField
+                        label="Bio"
+                        defaultValue={user?.bio}
+                        value={bio}
+                        onChange={(e) => setBio(e.target.value)}
+                        disabled={isUpdatingProfile}
+                        className="min-h-[100px]"
+                    />
+
+                    <div className="mt-6 flex justify-end">
+                        <Button
+                            disabled={isUpdatingProfile || isButtonDisabled}
+                            type="submit"
+                        >
+                            {isUpdatingProfile && <IconLoader />} Save Changes
+                        </Button>
+                    </div>
+                </form>
+            </Section>
+
+            <Section
+                title="Themes"
+                description="Customize the look and feel of the website"
+                className="border-t border-border pt-8"
+                contentClassName="border border-border rounded-md p-4"
+            >
+                <ThemeList />
+            </Section>
+
+            <Section
+                title="Disabling your Let's Live account"
+                description="Completely deactivate your account"
+                className="border-t border-border pt-8"
+                contentClassName="border border-border rounded-md p-4"
+            >
+                <div className="flex w-full items-center justify-between">
+                    <p className="w-2/3 text-sm text-destructive">
+                        When you disable your account, your profile and
+                        notifications will be hidden, and your account will be
+                        deactivated. You can reactivate your account at any
+                        time.
+                    </p>
+                    <button className="rounded-md bg-destructive px-4 py-2 text-sm font-medium text-destructive-foreground hover:bg-destructive-hover">
+                        Disable Your Let&apos;s Live Account
+                    </button>
+                </div>
+            </Section>
+        </>
+    );
 }
