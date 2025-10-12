ab660d3946a3e5a25a51a6a469a555fcdc8ca804
ThNam203 <hthnam203@gmail.com>
2025-10-13 02:08:22 +0700

    feat: add social media section ui

--- ui/components/icons/base-icon.tsx
+++ ui/components/icons/base-icon.tsx
@@ -1,25 +1,28 @@
-import { defaultIconProps, IconProp } from "@/types/icon-prop";
+"use client";
+
+import { getDefaultIconProps, IconProp } from "@/types/icon-prop";
+import { useTheme } from "next-themes";
 
 type BaseIconComponentProps = IconProp & { children: React.ReactNode };
 
-export function BaseIcon({
-    color = defaultIconProps.color,
-    width = defaultIconProps.width,
-    height = defaultIconProps.height,
-    className = defaultIconProps.className,
-    viewBox = defaultIconProps.viewBox,
-    children,
-    ...props
-}: BaseIconComponentProps) {
+export function BaseIcon(rawProps: BaseIconComponentProps) {
+    const { resolvedTheme } = useTheme();
+
+    if (!resolvedTheme) return null;
+
+    const props = { ...getDefaultIconProps(resolvedTheme), ...rawProps };
+    const { color, width, height, className, viewBox, children, ...rest } =
+        props;
+
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
             width={width}
             height={height}
             className={className}
-            fill={color}
+            style={{ color }}
             viewBox={viewBox}
-            {...props}
+            {...rest}
         >
             {children}
         </svg>
