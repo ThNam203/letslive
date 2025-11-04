8acd42c79800998907b4c77f48a121cd6e341ac6
ThNam203 <hthnam203@gmail.com>
2025-11-04 21:47:51 +0700

    chore: merge user, auth and livestream databases into one
    closes #57

---
+++ docker-entrypoint-initdb.d/01-create-dbs.sql
@@ -0,0 +1,8 @@
+-- 1. Create the 'letslive_auth' database
+CREATE DATABASE letslive_auth;
+
+-- 2. Create the 'letslive_livestream' database
+CREATE DATABASE letslive_livestream;
+
+-- 3. Create the 'letslive_user' database
+CREATE DATABASE letslive_user;
