614635bd9224e1f8ddbdff0fca953c1eb28d6b9b
ThNam203 <hthnam203@gmail.com>
2025-12-27 20:17:54 +0700

    feat: integrate Sentry for error tracking and logging in the application
    - Added Sentry configuration for client, server, and edge environments to capture errors and performance metrics.
    - Introduced global error handling to capture exceptions and log them to Sentry.
    - Updated package.json and package-lock.json to include the Sentry SDK.
    - Enhanced .gitignore to exclude Sentry configuration files.
    
    This integration aims to improve error monitoring and enhance the overall reliability of the application.
    
    closes #52

---
+++ ui/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://990ba0f96a5ad34873933929847aaf41@o4510607010299904.ingest.de.sentry.io/4510607015084112",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
