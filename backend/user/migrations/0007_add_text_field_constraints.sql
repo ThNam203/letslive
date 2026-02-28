de6ffc417e88033a8bd45ac6ba307e9d15bfce01
ThNam203 <hthnam203@gmail.com>
2026-02-28 14:26:31 +0700

    fix constaint migration files
    - Updated email, password_hash, and other fields in auths, livestreams, users, and related tables to have VARCHAR type with specified length limits.
    - Added corresponding DOWN migrations to revert fields to TEXT type for each table.
    
    These changes enhance data integrity by enforcing length constraints on text fields across the application.

--- backend/user/migrations/0007_add_text_field_constraints.sql
+++ backend/user/migrations/0007_add_text_field_constraints.sql
@@ -1,3 +1,4 @@
+-- +goose Up
 ALTER TABLE users ALTER COLUMN email TYPE VARCHAR(320);
 ALTER TABLE users ALTER COLUMN profile_picture TYPE VARCHAR(2048);
 ALTER TABLE users ALTER COLUMN background_picture TYPE VARCHAR(2048);
@@ -7,3 +8,14 @@ ALTER TABLE livestream_information ALTER COLUMN thumbnail_url TYPE VARCHAR(2048)
 ALTER TABLE user_social_links ALTER COLUMN url TYPE VARCHAR(2048);
 
 ALTER TABLE notifications ALTER COLUMN action_url TYPE VARCHAR(2048);
+
+-- +goose Down
+ALTER TABLE users ALTER COLUMN email TYPE TEXT;
+ALTER TABLE users ALTER COLUMN profile_picture TYPE TEXT;
+ALTER TABLE users ALTER COLUMN background_picture TYPE TEXT;
+
+ALTER TABLE livestream_information ALTER COLUMN thumbnail_url TYPE TEXT;
+
+ALTER TABLE user_social_links ALTER COLUMN url TYPE TEXT;
+
+ALTER TABLE notifications ALTER COLUMN action_url TYPE TEXT;
