4580aa0018442e617151eb70e956a2cbe3c3c13b
ThNam203 <hthnam203@gmail.com>
2026-03-17 00:57:39 +0700

    chore: fix sidebar layout

---
+++ web/app/[lng]/(main)/_components/main-body-layout.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import LeftBar from "@/components/leftbar/left-bar";
+
+export function MainBodyLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <ResizablePanelGroup orientation="horizontal">
+            <LeftBar />
+            <ResizableHandle />
+            <ResizablePanel
+                id="2"
+                className="bg-background min-h-0"
+            >
+                {children}
+            </ResizablePanel>
+        </ResizablePanelGroup>
+    );
+}
