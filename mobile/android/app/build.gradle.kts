e64d63b66e972d2e268b60d767cb998b8c1d30b3
ThNam203 <hthnam203@gmail.com>
2026-03-18 00:04:50 +0700

    chore: update package names

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.letslive"
+    namespace = "sen1or.letslive"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.letslive"
+        applicationId = "sen1or.letslive"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- mobile/android/app/src/main/kotlin/sen1or/letslive/MainActivity.kt
+++ mobile/android/app/src/main/kotlin/sen1or/letslive/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.letslive
+package sen1or.letslive
 
 import io.flutter.embedding.android.FlutterActivity
 
