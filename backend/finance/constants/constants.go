658a97898bdd5022e6072274ce404fd0858ece39
ThNam203 <hthnam203@gmail.com>
2026-03-31 11:02:57 +0700

    feat: integrate finance service with shared package
    - Updated Dockerfile to use Go 1.26.1 and adjusted build context for finance service.
    - Modified go.mod and go.sum to include shared package dependencies and updated various library versions.
    - Refactored HTTP handlers and middlewares to utilize shared components for logging and request ID management.
    - Removed unused constants and middleware files, streamlining the finance service structure.
    - Enhanced configuration management by implementing a post-processing function for database connection strings.

--- backend/finance/constants/constants.go
+++
@@ -1,3 +0,0 @@
-package constants
-
-const CONFIG_SERVER_DEFAULT_RELOAD_INTERVAL = 60000 // milli
--- backend/finance/handlers/general/route_not_found.go
+++ backend/finance/handlers/general/route_not_found.go
@@ -3,8 +3,9 @@ package general
 import (
 	"net/http"
 	"sen1or/letslive/finance/response"
+	sharedresponse "sen1or/letslive/shared/response"
 )
 
 func (h *GeneralHandler) RouteNotFoundHandler(w http.ResponseWriter, r *http.Request) {
-	h.WriteResponse(w, r.Context(), response.NewResponseFromTemplate[any](response.RES_ERR_ROUTE_NOT_FOUND, nil, nil, nil))
+	h.WriteResponse(w, r.Context(), sharedresponse.NewResponseFromTemplate[any](response.RES_ERR_ROUTE_NOT_FOUND, nil, nil, nil))
 }
