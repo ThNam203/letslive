dd37b5e140e70f63a368ac1b3c7a46651ab9235c
ThNam203 <hthnam203@gmail.com>
2026-04-22 15:16:26 +0700

    refactor(use-translation): simplify language change logic and remove unused state
    - Removed the `activeLng` state and its associated effect, streamlining the language change process.
    - Updated the language change condition to ensure it only triggers when necessary, improving performance and clarity.

--- web/hooks/use-translation.ts
+++ web/hooks/use-translation.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import i18next from "@/lib/i18n/i18next";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { UseTranslationOptions } from "react-i18next";
 import { useParams, usePathname } from "next/navigation";
@@ -17,21 +17,13 @@ function useT(
     if (typeof lng !== "string")
         throw new Error("useT is only available inside /app/[lng]");
 
-    const [activeLng, setActiveLng] = useState(i18next.resolvedLanguage);
-
-    useEffect(() => {
-        if (runsOnServerSide) return;
-        if (activeLng === i18next.resolvedLanguage) return;
-        queueMicrotask(() => setActiveLng(i18next.resolvedLanguage));
-    }, [activeLng, i18next.resolvedLanguage]);
-
     useEffect(() => {
         if (runsOnServerSide) return;
         if (!lng || i18next.resolvedLanguage === lng) return;
         i18next.changeLanguage(lng);
     }, [lng]);
 
-    if (runsOnServerSide && i18next.resolvedLanguage !== lng) {
+    if (i18next.resolvedLanguage !== lng) {
         i18next.changeLanguage(lng);
     }
 
